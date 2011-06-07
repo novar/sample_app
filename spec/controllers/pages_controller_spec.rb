f0402e785b667490d007d6ada27f14d135e5553e
mrouse <mrouseska@gmail.com>
2011-06-07 17:45:56 +0100

    Help page added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -40,6 +40,15 @@ render_views
   end
 end
 
-
+  describe "GET 'help'" do
+    it "should be successful" do
+    get 'help'
+    response.should be_success
+  end
+    it "should have the right title" do
+    get  'help'
+    response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+  end
+end
 
 end
